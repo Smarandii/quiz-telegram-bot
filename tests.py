41ceca7686e224b94626a8620fa47fa5bb41e667
Oleg Smarandi <48328325+Smarandii@users.noreply.github.com>
2020-08-30 18:00:38 +0300

    Delete tests.py

--- tests.py
+++
@@ -1,3 +0,0 @@
-data_base = ['1,231584958,None,None,Olejius,None,None,q1- q2- q3- q4- q5- q6-', '0,00000000000,Nul,Non,NullUser,00000000000000,00000,000000', '-1,blalbalba, blablabla, blalba']
-data_base = sorted(data_base)
-print(data_base)
